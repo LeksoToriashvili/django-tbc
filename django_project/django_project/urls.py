d5ec0262e80fd136637e3181631b8799ae57b0f2
LeksoToriashvili <ltoriashvili@gmail.com>
2024-10-16 02:10:39 +0400

    did some work

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -14,13 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from doctest import debug
 from xml.etree.ElementInclude import include
 
+import debug_toolbar
+from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from django_project import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
     path('order/', include('order.urls')),
-]
+] + debug_toolbar_urls()
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django_project/store/models.py
+++ django_project/store/models.py
@@ -13,9 +13,15 @@ class Category(models.Model):
     def parent_name(self):
         return self.parent.name if self.parent else "No Parent"
 
+    @property
+    def has_parent(self):
+        return self.parent is not None
+
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    price = models.PositiveSmallIntegerField(default=0)
     category = models.ManyToManyField(Category, related_name='products')
     description = models.TextField(verbose_name="Detailed description of product.", null=True, blank=True)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
