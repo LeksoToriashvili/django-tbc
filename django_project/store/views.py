58c6667b0dcb48a7a1c32dac4885408ce3941ce7
LeksoToriashvili <ltoriashvili@gmail.com>
2024-10-17 16:23:54 +0400

    Simple HTML Rendering task completed

--- django_project/store/views.py
+++ django_project/store/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from store.models import Category, Product
+from django.db.models import F, Sum
+
+
+def get_all_subcategories(category, visited=None):
+    if visited is None:
+        visited = set()
+
+    if category in visited:
+        return []
+
+    visited.add(category)
+
+    subcategories = category.children.all()
+    all_subcategories = list(subcategories)
+
+    for subcategory in subcategories:
+        all_subcategories.extend(get_all_subcategories(subcategory, visited))
+
+    return all_subcategories
 
 
 def categories(request):
@@ -19,8 +38,27 @@ def category(request):
 
 
 def category_products(request, category_id):
-    l = []
-    parent_category = Category.objects.get(id=category_id)
-    subcategories = parent_category.children.all()
+    category = Category.objects.get(id=category_id)
+    all_subcategories = get_all_subcategories(category)
+    category_names = [name for name in all_subcategories]
+    products = Product.objects.filter(category__name__in=category_names)
+    prices = list(products.values_list('price', flat=True))
+    print(prices)
+    expensive = max(prices)
+    cheap = min(prices)
+    avg = sum(prices) / len(prices)
+    total = products.aggregate(total=Sum(F('price') * F('quantity')))['total']
 
-    return JsonResponse({'all_products': list(subcategories.values())})
+    return render(request, 'category_product_listing.html', {
+        'category': category.name,
+        'products': products,
+        'expensive': expensive,
+        'cheap': cheap,
+        'avg': avg,
+        'total': total,
+    })
+
+
+def product_details(request, product_id):
+    product = Product.objects.prefetch_related('category').get(id=product_id)
+    return render(request, 'product_details.html', {'product': product})
